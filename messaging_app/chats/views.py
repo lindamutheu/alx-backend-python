85488fd8e5650dcec6594d012372db7884c07b33
Alpha Infinos <kevinmullerhi@gmail.com>
2025-06-01 02:39:11 +0300

    chats

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        # Add the request user to the conversation participants if desired
+        conversation.participants.add(self.request.user)
+        conversation.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get("conversation_pk")
+        return Message.objects.filter(conversation_id=conversation_id)
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get("conversation_pk")
+        serializer.save(
+            sender=self.request.user,
+            conversation_id=conversation_id
+        )
 # Create your views here.
