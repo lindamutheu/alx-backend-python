c823122c6e8f8a77fc8469afc1aa610667dd4e6c
Alpha Infinos <kevinmullerhi@gmail.com>
2025-06-01 03:39:10 +0300

    chats/urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,6 +4,7 @@ from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 import rest_framework.routers as routers
 
+
 # Top-level router
 router = routers.DefaultRouter() 
 router.register(r'conversations', ConversationViewSet, basename='conversation')
